cde5ca6fd6d13128dde3061cf65028c54f28216b
Harshitha Basireddyvari <harshitha.basireddy29@gmail.com>
2024-10-22 10:27:50 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Introduction from "./Introduction";
+import AddCustomer from "./AddCustomer";
+import ListCustomer from "./ListCustomer";
+import "./index.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Commit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1 className="title">Customer Management System</h1>
+        <nav className="nav-links">
+          <Link to="/introduction" className="link">
+            Introduction
+          </Link>
+
+          <Link to="/add-customer" className="link">
+            Add Customer
+          </Link>
+
+          <Link to="/list-customers" className="link">
+            List Customers
+          </Link>
+        </nav>
+        <div className="content-box">
+          <Routes>
+            <Route path="/introduction" element={<Introduction />} />
+            <Route path="/add-customer" element={<AddCustomer />} />
+            <Route path="/list-customers" element={<ListCustomer />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
